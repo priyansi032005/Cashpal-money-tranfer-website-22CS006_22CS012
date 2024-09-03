a34c74b0b0de20fed48c7a78e160b90f31565977
priyansi Borda <priyansi032005@gmail.com>
2024-09-03 09:29:40 +0530

    second commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Aena. All Rights Reserved.</p>
+      <p>&copy; 2024 CashPal. All Rights Reserved.</p>
     </footer>
   );
 }
