8fac723b46156489ddff4421488ab087f95c174d
priyansi Borda <priyansi032005@gmail.com>
2024-09-07 20:43:16 +0530

    Poatman API fetch

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/ping', (req, res) => {
-    res.send('PONG');
+app.get("/ping", (req, res) => {
+  res.send("PONG");
 });
 
 app.use(bodyParser.json());
 app.use(cors());
-// app.use('/auth', AuthRouter);
-// app.use('/products', ProductRouter);
+app.use("/auth", AuthRouter);
+// app.use("/products", ProductRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
+  console.log(`Server is running on ${PORT}`);
+});
